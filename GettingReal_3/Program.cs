c6368a00e8f5eb85de9c02c005b34539967c0c46
Søren Gjedsøe Eriksen <soer33b0@edu.eal.dk>
2018-11-27 09:43:08 +0100

    lidt tilføjet til menu

--- GettingReal_3/Program.cs
+++ GettingReal_3/Program.cs
@@ -11,33 +11,43 @@ namespace GettingReal_3
         static void Main(string[] args)
         {
             Console.WriteLine("Vælg punkt mellem 1-4");
-            Console.WriteLine("abe");
+            Console.WriteLine("1: Registrér timer");
+            Console.WriteLine("2: Se afsluttede vagter for denne måned");
+            Console.WriteLine("3: Administration");
+            Console.WriteLine("4: Luk ned");
+
             int switchCase = Convert.ToInt32(Console.ReadLine());
             switch (switchCase)
             {
                 case 1:
+                    Console.Clear();
                     Console.WriteLine("1. Registrér timer");
 
 
                     break;
 
                 case 2:
+                    Console.Clear();
                     Console.WriteLine("2. Se afsluttede vagter for denne måned");
 
                     break;
 
                 case 3:
+                    Console.Clear();
                     Console.WriteLine("3. Administration");
 
                     break;
 
                 case 4:
+                    Console.Clear();
                     Console.WriteLine("4. Luk ned");
                     //hej
                     break;
 
             }
         }
+    }
+}
 
 
         
