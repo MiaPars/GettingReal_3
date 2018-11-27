b9615e70f055a5ddb26fde1f8d21cc8df3b76139
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-11-27 22:27:51 +0100

    metoder inde i menu

--- GettingReal_3/Store.cs
+++ GettingReal_3/Store.cs
@@ -8,6 +8,52 @@ namespace GettingReal_3
 {
     public class Store
     {
-          
+        List<string> storeList = new List<string>();
+
+        public void AddStore()
+        {
+            storeList.Add("nørregade");
+            storeList.Add("thomas gade");
+        }
+        public bool CheckStore(string store)
+        {
+            foreach (string item in storeList)
+            {
+                if (store.Equals(item))
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+
+        }
+        public string storeName {
+
+            get
+            {
+                return storeName;
+            }
+            set
+            {
+                foreach(string item in storeList)
+                {
+                    if (storeName.Equals(item))
+                    {
+                        storeName = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("du har intastet forkert butiknavn");
+                        Menu m = new Menu();
+                        m.Show();
+                        
+                    }
+                }
+            }
+
+        }
+
+        
     }
 }
